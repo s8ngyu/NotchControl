d3f152c7d57ae7997431e33fc90d1f9889690078
peterprd <djperter@outlook.com>
2019-06-23 02:18:37 +0900

    header

--- ncprefs/NCRootListController.h
+++ ncprefs/NCRootListController.h
@@ -5,7 +5,15 @@
 #import <Cephei/HBPreferences.h>
 #import <spawn.h>
 
-@interface NCRootListController : HBRootListController
+@interface NCRootListController : HBRootListController {
+    UITableView * _table;
+}
+@property (nonatomic, retain) UIView *headerView;
+@property (nonatomic, retain) UIImageView *headerImageView;
+@property (nonatomic, retain) UILabel *bannerAuthorLabel;
+@property (nonatomic, retain) UILabel *bannerTitleLabel;
+@property (nonatomic, retain) UILabel *titleLabel;
+@property (nonatomic, retain) UIImageView *iconView;
 - (void)respring:(id)sender;
 - (void)resetPrefs:(id)sender;
 @end
